e4360c6d800c745c6a36ca478eb1030ca063fbaa
MGzhou <myboy111@gmail.com>
2024-11-23 22:00:13 +0800

    update

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from dgocr.dgocr import DGOCR
 rec_path = r"models/large/recognition_model_general"  # 文字识别模型路径
 det_path = r"models/large/detection_model_general/model_1600x1600.onnx"  # 文本框检测模型文件路径
 img_size=1600       # 文本框检测模型的输入图片大小限制
-cpu_thread_num=2    # onnx 运行线程数
+cpu_thread_num=4    # onnx 运行线程数, 线程越多，识别速度越快
 
 # 初始化模型
 ocr = DGOCR(rec_path, det_path, img_size, cpu_thread_num=cpu_thread_num)
--- demo_seglink.py
+++ demo_seglink.py
@@ -8,7 +8,7 @@ rec_path = r"models/large/recognition_model_general"  # 文字识别模型路径
 det_path = r"models\base_seglink++\detection_model_general\model_1024x1024.onnx"  # 文本框检测模型文件路径
 img_size=1024           # 文本框检测模型的输入图片大小限制
 model_type = "seglink"  # 模型类型
-cpu_thread_num=2        # onnx 运行线程数
+cpu_thread_num=4        # onnx 运行线程数, 线程越多，识别速度越快
 # 初始化模型
 ocr = DGOCR(rec_path, det_path, img_size=img_size, model_type=model_type, cpu_thread_num=cpu_thread_num)
 
